088fe90dca3f7ac4a6064bd866c2a770a950ed97
cdemeo92 <demeoclaudio92@gmail.com>
2026-07-14 17:55:36 +0200

    refactor: implement centralized Content Security Policy and secure headers plug

---
+++ lib/daily_logos_web/plugs/security_headers.ex
@@ -0,0 +1,30 @@
+defmodule DailyLogosWeb.Plugs.SecurityHeaders do
+  @moduledoc """
+  Applies secure browser headers with a centralized Content Security Policy.
+  """
+  import Phoenix.Controller, only: [put_secure_browser_headers: 2]
+
+  @base_csp [
+    "default-src 'self'",
+    "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+    "font-src 'self' https://fonts.gstatic.com",
+    "img-src 'self' data: https:",
+    "connect-src 'self' https://www.google-analytics.com https://region1.analytics.google.com https://stats.g.doubleclick.net",
+    "frame-src 'self' https://docs.google.com https://www.googletagmanager.com"
+  ]
+
+  @csp Enum.join(
+         if(Mix.env() == :prod,
+           do: @base_csp ++ ["upgrade-insecure-requests"],
+           else: @base_csp
+         ),
+         "; "
+       )
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    put_secure_browser_headers(conn, %{"content-security-policy" => @csp})
+  end
+end
--- lib/daily_logos_web/router.ex
+++ lib/daily_logos_web/router.ex
@@ -9,14 +9,14 @@ defmodule DailyLogosWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {DailyLogosWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug DailyLogosWeb.Plugs.SecurityHeaders
   end
 
   pipeline :locale_set do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug DailyLogosWeb.Plugs.SecurityHeaders
   end
 
   pipeline :api do
