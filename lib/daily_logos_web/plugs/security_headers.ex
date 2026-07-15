e3bc11fc7d0b647806f5959dd59720b8e5a33ed6
cdemeo92 <demeoclaudio92@gmail.com>
2026-07-15 15:03:50 +0200

    fix: update Content Security Policy to include additional sources for styles and connections

--- lib/daily_logos_web/plugs/security_headers.ex
+++ lib/daily_logos_web/plugs/security_headers.ex
@@ -7,10 +7,10 @@ defmodule DailyLogosWeb.Plugs.SecurityHeaders do
   @base_csp [
     "default-src 'self'",
     "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com",
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.googletagmanager.com",
     "font-src 'self' https://fonts.gstatic.com",
     "img-src 'self' data: https:",
-    "connect-src 'self' https://www.google-analytics.com https://region1.analytics.google.com https://stats.g.doubleclick.net",
+    "connect-src 'self' https://www.google-analytics.com https://region1.analytics.google.com https://region1.google-analytics.com https://stats.g.doubleclick.net",
     "frame-src 'self' https://docs.google.com https://www.googletagmanager.com"
   ]
 
